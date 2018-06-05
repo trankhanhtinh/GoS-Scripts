30734a35bf24cfa601c29921e2848f4bb29bb28c
Ark223 <Ark223Neww@gmail.com>
2018-06-05 13:23:37 +0200

    JustEvade External v1.0.5.2 BETA

--- JustEvade External.lua
+++ JustEvade External.lua
@@ -1166,18 +1166,18 @@ function JustEvade:OnProcessSpell()
 			local SRange = SpellDet.range
 			if SType == "linear" or SType == "conic" then
 				if SpellDet.displayName == "Steel Tempest" or SpellDet.displayName == "Steel Wind Rising" or SpellDet.displayName == "Gathering Storm" or SpellDet.displayName == "Death Sentence" then
-					local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):normalized()*(-SRange)
+					local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):Normalized()*(-SRange)
 					s = {slot = SpellDet.slot, source = unit, startTime = Game.Timer(), startPos = Vector(spell.startPos), endPos = Vector(endPos), name = spell.name}
 					TableInsert(self.DetSpells, s)
 				else
-					local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):normalized()*SRange
+					local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):Normalized()*SRange
 					s = {slot = SpellDet.slot, source = unit, startTime = Game.Timer(), startPos = Vector(spell.startPos), endPos = Vector(endPos), name = spell.name}
 					TableInsert(self.DetSpells, s)
 				end
 			elseif SType == "circular" or SType == "rectangular" or SType == "annular" then
 				if SRange > 0 then
 					if GetDistance(unit.pos, spell.placementPos) > SRange then
-						local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):normalized()*SRange
+						local endPos = Vector(spell.startPos)-Vector(Vector(spell.startPos)-spell.placementPos):Normalized()*SRange
 						s = {slot = SpellDet.slot, source = unit, startTime = Game.Timer(), startPos = Vector(spell.startPos), endPos = Vector(endPos), name = spell.name}
 						TableInsert(self.DetSpells, s)
 					else
